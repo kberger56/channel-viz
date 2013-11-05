2a6c6a9dfab7cb12a1f2cb3fd4b5fbe0287d1425
kberger56 <regreb56@yahoo.com>
2013-11-05 15:09:43 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -17,7 +17,7 @@
 		applicationName	= 'Temperature Sensor Readings', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 120, // Default interval for data to be displayed (in seconds)
-		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
+		dataColor		= '3366CC', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
